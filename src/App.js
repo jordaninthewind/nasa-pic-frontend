356c87421f090fbd382f73271ef085d5e9e4c825
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-21 11:36:38 -0800

    ERROR persist

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import PhotosContainer from './containers/PhotosContainer'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import NavBar from './containers/Navbar'
+
 
 class App extends Component {
   render() {
